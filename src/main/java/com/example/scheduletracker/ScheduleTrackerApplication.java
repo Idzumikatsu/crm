017606f609fc3a11b07bfd4bd0554b13fccd15c7
Admin <pwpvpdru@yandex.ru>
2025-05-30 23:43:36 +0300

    init

---
+++ src/main/java/com/example/scheduletracker/ScheduleTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.example.scheduletracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleTrackerApplication.class, args);
+    }
+}
