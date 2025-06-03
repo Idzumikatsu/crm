3e15573e5adb4cdf530776c4af2ccdf36778a2eb
Idzumikatsu <49922673+Idzumikatsu@users.noreply.github.com>
2025-06-03 22:35:52 +0300

    Connect login page with security

--- src/main/java/com/example/scheduletracker/config/SecurityConfig.java
+++ src/main/java/com/example/scheduletracker/config/SecurityConfig.java
@@ -1,37 +1,49 @@
 // src/main/java/com/example/scheduletracker/config/SecurityConfig.java
 package com.example.scheduletracker.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // Отключаем CSRF для простоты (только в dev!)
                 .csrf(csrf -> csrf.disable())
-                // Разрешаем без логина доступ к Swagger и публичным GET-эндпоинтам
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/favicon.ico",
-                                "/",       // если хотите, чтобы корень был публичным
+                                "/login.html",
+                                "/", // если хотите, чтобы корень был публичным
                                 "/api/teachers/**",
                                 "/api/groups/**",
                                 "/api/lessons/**"
                         ).permitAll()
-                        // всё остальное — под логин (требует аутентификацию)
                         .anyRequest().authenticated()
                 )
-                // включаем HTTP Basic для остальных
+                .userDetailsService(userDetailsService)
+                .formLogin(form -> form
+                        .loginPage("/login.html")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login.html")
+                        .permitAll()
+                )
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
