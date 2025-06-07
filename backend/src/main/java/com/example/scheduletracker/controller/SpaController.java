882b63e935cc3661927f0c2e87288bc5ce60cc94
Idzumikatsu <49922673+Idzumikatsu@users.noreply.github.com>
2025-06-07 19:27:50 +0300

    fix: update SPA controller mapping (#188)

--- backend/src/main/java/com/example/scheduletracker/controller/SpaController.java
+++ backend/src/main/java/com/example/scheduletracker/controller/SpaController.java
@@ -1,17 +1,17 @@
 package com.example.scheduletracker.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SpaController {
 
-    @GetMapping(value = "/{path:^(?!api|swagger-ui$).*$}")
-    public String redirectRoot() {
+    @RequestMapping("/")
+    public String redirectIndex() {
         return "forward:/index.html";
     }
 
-    @GetMapping(value = "/**/{path:^(?!api|swagger-ui$)[^\\.]*}")
+    @RequestMapping("/{*path:^(?!api|swagger-ui).*}")
     public String redirect() {
         return "forward:/index.html";
     }
