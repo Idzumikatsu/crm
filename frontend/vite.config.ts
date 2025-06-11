6ba9df7537b449afcf82a3227a4ee53eacf90fd9
Idzumikatsu <49922673+Idzumikatsu@users.noreply.github.com>
2025-06-11 11:44:29 +0300

    Update Vite config with proxy settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   envPrefix: 'VITE_',
-  server: {
-    proxy: {
-      '/api': 'https://crm-synergy.ru',
-    },
-  },
+  server: { proxy: { '/api': 'http://localhost:8080' } },
 })
